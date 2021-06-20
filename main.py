9e94abf8783e84bf0ff390791ab90bcfe46bedb6
Spyros Siannas <ssiannas@gmail.com>
2021-06-20 11:43:42 +0300

    Finished part A

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.impute import LinearRegression
+
 
 if __name__ == "__main__":
     df = pd.read_csv("dataset/healthcare-dataset-stroke-data/healthcare-dataset-stroke-data.csv")
-    df.plot(x="id", y=["bmi"])
-    plt.show()
+    dataset3 = df
+    x = dataset3.age.values
+    x = x.reshape(x.shape[0],1)
+    y = dataset3.bmi
+
+    y = y.values.reshape(y.shape[0], 1)
+
+    lreg = LinearRegression()
+    lreg.fit(x , y)
